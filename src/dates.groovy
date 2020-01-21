be89da2fb7b1cb7bcd429d0ff2cb1cfc002b1434
swi18 <stuart.wilson@sky.uk>
2020-01-21 19:11:48 +0000

    Reformat of script

--- src/dates.groovy
+++ src/dates.groovy
@@ -1,51 +1,18 @@
 @groovy.lang.Grab(group = 'org.seleniumhq.selenium', module = 'selenium-java', version = '3.141.59')
 @groovy.lang.Grab(group = 'com.fasterxml.jackson.core', module = 'jackson-databind', version = '2.10.1')
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.databind.util.StdDateFormat
-import org.openqa.selenium.chrome.ChromeDriver
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
 import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.remote.RemoteWebDriver
 
 import java.text.SimpleDateFormat
+import java.util.concurrent.TimeUnit
 
 System.setProperty('webdriver.chrome.driver', '../chromedriver')
 
-private static ArrayList<BinData> getAllBinDates(ChromeDriver browser) {
-    def bds = new ArrayList<BinData>()
-    browser.findElementByClassName("bins").getText().split('\n').each {
-        if (it.contains('Grey') || it.contains('Blue') || it.contains('Brown') || it.contains('Green') || it.contains('Glass')) {
-            String[] results = it.split("([b|B]in)|Box")
-            bds.add(new BinData(colour: results[0].trim(), collectionDate: convertToDate(results[1].trim())))
-        }
-    }
-    bds
-}
-
-static BinData getFirstCollectionDate(ArrayList<BinData> bds) {
-    bds.sort { a, b -> a.collectionDate <=> b.collectionDate }.get(0)
-}
-
 static Date convertToDate(String date) {
     new SimpleDateFormat("EEEE dd MMMM yyyy").parse(date)
-}
-
-static writeSchedule(ArrayList<BinData> schedule) {
-    final ObjectMapper mapper = new ObjectMapper()
-    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-    mapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true))
-
-    def dir = new File(String.format("%s/.wheelie", System.getProperty("user.home")))
-    if (!dir.exists()) dir.mkdirs()
-
-    def scheduleFile = new File(String.format("%s/%s", dir, "schedule.json"))
-    scheduleFile.text = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(schedule)
-}
-
-static ChromeDriver getBrowser() {
-    def browser = new ChromeDriver(new ChromeOptions().addArguments("--headless"))
-    browser.manage().window().maximize()
-    browser.get('https://www.stirling.gov.uk/bins-waste-recycling/bin-collection-dates/?uprn=122014510')
-    browser
 }
 
 class BinData {
@@ -54,9 +21,54 @@ class BinData {
     boolean toBeCollected
 }
 
-ChromeDriver browser = getBrowser()
-def schedule = getAllBinDates(browser)
-def firstCollectionDate = getFirstCollectionDate(schedule)
+///////////////////////////////////////////////////////////////////
+// Get a web browser and navigate to the first page
+///////////////////////////////////////////////////////////////////
+
+DesiredCapabilities capabilities = DesiredCapabilities.chrome()
+
+def options = new ChromeOptions()
+options.addArguments("disable-infobars")
+capabilities.setCapability(ChromeOptions.CAPABILITY, options)
+
+RemoteWebDriver driver = new RemoteWebDriver(capabilities)
+
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+
+def dir = new File(String.format("%s/.wheelie", System.getProperty("user.home")))
+if (!dir.exists()) dir.mkdirs()
+def customer = new File(String.format("%s/%s", dir, "customer.json"))
+
+driver.get(new JsonSlurper().parseText(customer.text).lookupUrl as String)
+
+///////////////////////////////////////////////////////////////////
+// Get the schedule from the Stirling council website
+///////////////////////////////////////////////////////////////////
+
+def schedule = new ArrayList<BinData>()
+driver.findElementByClassName("bins").getText().split('\n').each {
+    if (it.contains('Grey') || it.contains('Blue') || it.contains('Brown') || it.contains('Green') || it.contains('Glass')) {
+        String[] results = it.split("([b|B]in)|Box")
+        schedule.add(new BinData(colour: results[0].trim(), collectionDate: convertToDate(results[1].trim())))
+    }
+}
+
+///////////////////////////////////////////////////////////////////
+// Get the first collection date
+///////////////////////////////////////////////////////////////////
+def firstCollectionDate = schedule.sort { a, b -> a.collectionDate <=> b.collectionDate }.get(0)
+
+///////////////////////////////////////////////////////////////////
+// Get all bins to be collected on the day
+///////////////////////////////////////////////////////////////////
 schedule.findAll { it.collectionDate == firstCollectionDate.collectionDate }.each { it.toBeCollected = true }
-writeSchedule(schedule)
-browser.quit()
+
+///////////////////////////////////////////////////////////////////
+// Record the result for other scripts to read
+///////////////////////////////////////////////////////////////////
+def json_str = JsonOutput.toJson(schedule)
+def json_beauty = JsonOutput.prettyPrint(json_str)
+File file = new File(String.format("%s/%s", dir, "schedule.json"))
+file.write(json_beauty)
+
+driver.quit()
